787e45854ea109cfda321134cb17d971bb381d9a
Matt Rothenberg <mrothenberg@obede.onmicrosoft.com>
2018-04-13 10:06:33 -0400

    state refactor

--- store/index.js
+++ store/index.js
@@ -3,36 +3,35 @@ import Vuex from "vuex";
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      posts: {
-        foo: {
+      posts: [
+        {
           image: "https://source.unsplash.com/1400x900/?nature",
           slug: "foo"
         },
-        bar: {
+        {
           image: "https://source.unsplash.com/1400x900/?water",
           slug: "bar"
         },
-        baz: {
+        {
           image: "https://source.unsplash.com/1400x900/?tech",
           slug: "baz"
         }
-      }
+      ]
     },
     getters: {
-      getPostBySlug: state => id => state.posts[id],
+      getPostBySlug: state => slug => {
+        return state.posts.find(post => post.slug === slug);
+      },
       nextPost: state => slug => {
-        const posts = Object.entries(state.posts);
-        const matchingPostObject = posts.find(post => post[0] === slug);
-        const indexOfMatchingPost = posts.indexOf(matchingPostObject);
-        let indexOfNewPost;
+        const currentPost = state.posts.find(post => post.slug === slug);
+        const currentPostIndex = state.posts.indexOf(currentPost);
+        let nextPostIndex;
 
-        if (indexOfMatchingPost === posts.length - 1) {
-          indexOfNewPost = 0;
-        } else {
-          indexOfNewPost = indexOfMatchingPost + 1;
-        }
+        currentPostIndex === state.posts.length - 1
+          ? (nextPostIndex = 0)
+          : (nextPostIndex = currentPostIndex + 1);
 
-        return posts[indexOfNewPost][1];
+        return state.posts[nextPostIndex];
       }
     },
     mutations: {}
